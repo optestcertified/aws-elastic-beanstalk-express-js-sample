ad660b099cfdfe4ef97a47d65968e1dab2ccd674
Opeoluwa Akinsiku <oakinsiku@inlaks.com>
2023-09-13 18:39:19 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('MY Cloud Job is here!'));
+app.get('/', (req, res) => res.send('Oh Lord my God, MY Cloud Job is here!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
